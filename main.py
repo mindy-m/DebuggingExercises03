cf90c014fd435284e7eaf90fd2f643d00db6a9e5
mindym1 <76831690+mindy-m@users.noreply.github.com>
2021-09-17 02:34:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# This program should convert points earned to a percent. Find and fix the logic errors.
+points_earned = 8
+points_possible = 10
+
+percentage = points_possible/points_earned * 10
+print(f"The student earned {points_earned} points out of {points_possible}, or {percentage}%.")
+
+# Here are some test cases:
+# Earning 8 out of 10 possible points = 80.0%. 
+# 11 of of 15 is 73.33333333333333%.
+# 23.4 out of 25 93.6%.
